8d67492ccbd44fabccf8b0f9811c80d31c99c945
Nirzon Pop <nirzon192@gmail.com>
2021-02-19 03:25:05 +0600

    Initial commit

---
+++ bin/dart.dart
@@ -0,0 +1,24 @@
+//import 'package:dart/dart.dart' as dart;
+
+import 'Rectangle.dart';
+import 'Shape.dart';
+
+void main(List<String> arguments) {
+
+  dynamic  Country=56.4;
+ /**/
+  double Tem=5.35;
+  num Age=13;
+  // String concatenation tripule qute
+  String Name=""" My name is
+  Faisal 
+  Mohammad """;
+  Tem=7;
+  Country="Hello";
+ // print('Hello world: ${age}!');
+
+  print (Country);
+  Shape shape= Rectangle(5, 5);
+  shape.area();
+  print(Name);
+}
